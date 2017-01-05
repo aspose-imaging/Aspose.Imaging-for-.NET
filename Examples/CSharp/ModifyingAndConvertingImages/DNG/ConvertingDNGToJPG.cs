343065260c090466f26bf92a39924cf02355911a
Ikram ul Haq <ikram.haq@aspose.com>
2017-01-05 10:34:47 +0500

    Imaging 16.12 release changes

--- Examples/CSharp/ModifyingAndConvertingImages/DNG/ConvertingDNGToJPG.cs
+++ Examples/CSharp/ModifyingAndConvertingImages/DNG/ConvertingDNGToJPG.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Aspose.Imaging.Examples.CSharp.ModifyingAndConvertingImages.DGN
+namespace Aspose.Imaging.Examples.CSharp.ModifyingAndConvertingImages.DNG
 {
-    class ConvertingDGNToJPG
+    class ConvertingDNGToJPG
     {
         public static void Run()
         {
