080ded88e1d3ddbda9b772ad64f64f7f14ebd650
Muhammad-Adnan-Ahmad <adnan.ahmad@aspose.com>
2018-08-31 01:40:24 +0500

    Added new feature as per Aspose.Imaging for .NET 18.8
    ChangeWindowSize.cs

---
+++ Examples/CSharp/ModifyingAndConvertingImages/PNG/ChangeWindowSize.cs
@@ -0,0 +1,28 @@
+﻿using Aspose.Imaging;
+using Aspose.Imaging.Examples.CSharp;
+using Aspose.Imaging.FileFormats.Png;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.ModifyingAndConvertingImages.PNG
+{
+	class ChangeWindowSize
+	{
+		public static void Run()
+		{
+			//ExStart:ChangeWindowSize
+			string dataDir = RunExamples.GetDataDir_PNG();
+			string sourceFile = @"test.png";
+			string outputFile = "result.png";
+			using (PngImage image = (PngImage)Image.Load(dataDir+sourceFile))
+			{
+				image.BinarizeBradley(10, 20);
+				image.Save(dataDir+outputFile);
+			}
+			//ExEnd:ChangeWindowSize
+
+		}
+	}
+}
