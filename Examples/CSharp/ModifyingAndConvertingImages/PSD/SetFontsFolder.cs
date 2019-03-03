d4f3174ac22f3442e10633bb8f06e087e9c87d81
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-03-03 13:00:56 +0500

    Added new feature
    Added new feature as per Aspose.Imaging for .NET 19.2

---
+++ Examples/CSharp/ModifyingAndConvertingImages/PSD/SetFontsFolder.cs
@@ -0,0 +1,33 @@
+﻿using Aspose.Imaging;
+using Aspose.Imaging.Examples.CSharp;
+using Aspose.Imaging.FileFormats.Psd;
+using Aspose.Imaging.ImageLoadOptions;
+using Aspose.Imaging.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.ModifyingAndConvertingImages.PSD
+{
+    class SetFontsFolder
+    {
+        public static void Run()
+        {
+            //ExStart:SetFontsFolder
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PSD();
+
+            string sourceFIle = dataDir+"grinched-regular-font.psd";
+            //Folder that contains fonts that we want to use for rendering 
+            //(file GrinchedRegular.otf must be in this folder for proper work of example)
+            Aspose.Imaging.FontSettings.SetFontsFolder(dataDir +"Fonts\\");
+            Aspose.Imaging.FontSettings.UpdateFonts();
+            using (PsdImage image = (PsdImage)Image.Load(sourceFIle, new PsdLoadOptions()))
+            {
+                image.Save(dataDir + "grinched-regular-font.png", new PngOptions());
+            }
+            //ExEnd:SetFontsFolder
+        }
+    }
+}
