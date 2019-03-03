1b1e8a76bbcbe6b30bc5216fd53c3a4c8c6ed26a
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-03-03 12:58:55 +0500

    Create CMXToPNGConversion.cs

---
+++ Examples/CSharp/ModifyingAndConvertingImages/CMX/CMXToPNGConversion.cs
@@ -0,0 +1,53 @@
+using Aspose.Imaging;
+using Aspose.Imaging.Examples.CSharp;
+using Aspose.Imaging.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.ModifyingAndConvertingImages.CMX
+{
+    class CMXToPNGConversion
+    {
+        public static void Run()
+        {
+
+            //ExStart:CMXToPNGConversion
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_CMX();
+
+            string[] fileNames = new string[] {
+             "Rectangle.cmx",
+             "Rectangle+Fill.cmx",
+             "Ellipse.cmx",
+             "Ellipse+fill.cmx",
+             "brushes.cmx",
+             "outlines.cmx",
+             "order.cmx",
+             "many_images.cmx",
+            };
+            foreach (string fileName in fileNames)
+            {
+                using (Image image = Image.Load(dataDir + fileName))
+                {
+                    image.Save(
+                     dataDir + fileName + ".docpage.png",
+                     new PngOptions
+                     {
+                         VectorRasterizationOptions =
+                       new CmxRasterizationOptions()
+                       {
+                           Positioning = PositioningTypes.DefinedByDocument,
+                           SmoothingMode = SmoothingMode.AntiAlias
+                       }
+                     });
+                }
+            }
+
+            //ExEnd:CMXToPNGConversion
+        }
+    }
+
+}
+
