cad41b2b5bc5d132b8d5fd463a90690b32a29524
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-05-01 02:20:16 +0500

    Added example w.r.t 19.4

---
+++ Examples/CSharp/ModifyingAndConvertingImages/CDR/SupportOfCDR.cs
@@ -0,0 +1,33 @@
+﻿using Aspose.Imaging;
+using Aspose.Imaging.Examples.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.ModifyingAndConvertingImages.CDR
+{
+    class SupportOfCDR
+    {
+        public static void Run() {
+
+            //ExStart:SupportOfCDR
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_CDR();
+
+            string inputFileName = dataDir + "test.cdr";
+            FileFormat expectedFileFormat = FileFormat.Cdr;
+            using (Image image = Image.Load(inputFileName))
+            {
+                if (expectedFileFormat != image.FileFormat)
+                {
+                    throw new Exception("Image FileFormat is not {expectedFileFormat}");
+                }
+            }
+
+
+            //ExEnd:SupportOfCDR
+
+        }
+    }
+}
