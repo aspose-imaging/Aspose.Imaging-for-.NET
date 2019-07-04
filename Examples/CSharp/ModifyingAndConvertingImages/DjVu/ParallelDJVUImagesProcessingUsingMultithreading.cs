16a2acc958bab2e749fe8ef889467e99462e84f4
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-07-04 21:12:40 +0500

    Added example w.r.t 19.6

---
+++ Examples/CSharp/ModifyingAndConvertingImages/DjVu/ParallelDJVUImagesProcessingUsingMultithreading.cs
@@ -0,0 +1,49 @@
+﻿using Aspose.Imaging;
+using Aspose.Imaging.Examples.CSharp;
+using Aspose.Imaging.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharp.ModifyingAndConvertingImages.DjVu
+{
+    class ParallelDJVUImagesProcessingUsingMultithreading
+    {
+
+        public static void Run() {
+
+            //ExStart:ParallelDJVUImagesProcessingUsingMultithreading
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_DjVu();
+            string fileName = "Sample.djvu";
+
+            int numThreads = 20;
+            var tasks = Enumerable.Range(1, numThreads).Select(
+                taskNum =>
+                {
+                    var inputFile = dataDir + fileName;
+                    var outputFile = dataDir + $"{fileName}_task{taskNum}.png";
+                    return Task.Run(
+                () =>
+                            {
+                                using (FileStream fs = File.OpenRead(inputFile))
+                                {
+                                    using (Image image = Image.Load(fs))
+                                    {
+                                        image.Save(outputFile, new PngOptions());
+                                    }
+                                }
+                            });
+                });
+            Task.WaitAll(tasks.ToArray());
+
+            //ExEnd:ParallelDJVUImagesProcessingUsingMultithreading
+
+        }
+    }
+}
