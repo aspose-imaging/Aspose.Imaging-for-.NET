8c5b802e6e1b6e92c0653c87d47f10d40240fd48
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-07-04 21:16:14 +0500

    Added example w.r.t 19.6

---
+++ Examples/CSharp/ModifyingAndConvertingImages/SVG/SVGToBMPConversion.cs
@@ -0,0 +1,38 @@
+﻿using Aspose.Imaging;
+using Aspose.Imaging.Examples.CSharp;
+using Aspose.Imaging.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.ModifyingAndConvertingImages.SVG
+{
+    class SVGToBMPConversion
+    {
+        public static void Run() {
+
+            //ExStart:SVGToBMPConversion
+            
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_SVG();
+
+            using (Image image = Image.Load(dataDir + "test.svg"))
+            {
+
+                BmpOptions options = new BmpOptions();
+                SvgRasterizationOptions svgOptions = new SvgRasterizationOptions();
+
+                svgOptions.PageWidth = 100;
+                svgOptions.PageHeight = 200;
+
+
+                options.VectorRasterizationOptions = svgOptions;
+
+                image.Save(dataDir + "test.svg_out.bmp", options);
+            }
+
+            //ExEnd:SVGToBMPConversion
+        }
+    }
+}
