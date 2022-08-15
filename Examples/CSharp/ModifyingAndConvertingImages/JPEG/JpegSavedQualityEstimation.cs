bacf9ef84455386a2f139509bfe8b4150c72c926
Samer El-Khatib <samer.el-khatib@aspose.com>
2022-08-15 11:37:57 +0300

    Release 22.8 - add example file

---
+++ Examples/CSharp/ModifyingAndConvertingImages/JPEG/JpegSavedQualityEstimation.cs
@@ -0,0 +1,29 @@
+﻿using Aspose.Imaging;
+using Aspose.Imaging.Examples.CSharp;
+using Aspose.Imaging.FileFormats.Jpeg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.ModifyingAndConvertingImages.JPEG
+{
+    class JpegSavedQualityEstimation
+    {
+        public static void Run()
+        {
+            //ExStart:AddThumbnailToEXIFSegment
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_JPEG();
+            Console.WriteLine("Running example JpegSavedQualityEstimation");
+            using (JpegImage image = (JpegImage)Image.Load(dataDir + "test.jpg"))
+            {
+                bool isNotDefaultQuality = image.JpegOptions.Quality != 75;
+            }
+
+            Console.WriteLine("Finished example JpegSavedQualityEstimation");
+            //ExEnd:AddThumbnailToEXIFSegmentk
+        }
+    }
+}
