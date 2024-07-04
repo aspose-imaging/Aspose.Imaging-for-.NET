8987c0293889a3e6779fe5f178a87e44b71d4cea
Samer El-Khatib <samer.el-khatib@aspose.com>
2024-07-04 15:54:31 +0300

    Release 24.8 - Update examples

---
+++ Examples/CSharp/ModifyingAndConvertingImages/DICOM/SupportDicomYBR422.cs
@@ -0,0 +1,34 @@
+﻿using Aspose.Imaging.Examples.CSharp;
+using Aspose.Imaging.Exif;
+using Aspose.Imaging.ImageOptions;
+using Aspose.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSharp.ModifyingAndConvertingImages.DICOM
+{
+    internal class SupportDicomYBR422
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Running example SupportDicomYBR422");
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_DICOM();
+
+            var inputPath = Path.Combine(dataDir, @"input.dcm");
+            using (var image = Image.Load(inputPath))
+            {
+                image.Save(inputPath + ".png");
+            }
+
+            File.Delete(inputPath + ".png");
+
+            Console.WriteLine("Finished example SupportDicomYBR422");
+        }        
+    }
+}
--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -353,6 +353,7 @@ namespace Aspose.Imaging.Examples.CSharp
             //// =====================================================
             //// =====================================================
             Console.WriteLine("Running modifying and converting images tests:");
+            SupportDicomYBR422.Run();
             RemoveAndSaveWithMetadata.Run();
             ModifyDicomTags.Run();
             KernelFilters.Run();
